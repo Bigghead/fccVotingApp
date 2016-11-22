a658f7f3af1f749f1d5ecc6da1c3452a4c55a130
Shav <shavenchew@yahoo.com>
2016-11-21 16:29:43 -0800

    'Bootstrap Navbar Partials'

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/', function(req, res){
   res.render('index');
 });
 
-app.listen('6000', function(){
+app.listen('5000', function(){
   console.log('Voting Site Live!');
 });
