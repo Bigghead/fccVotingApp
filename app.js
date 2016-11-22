eb2a0269457ca223890a2eb0471462bfeb98759a
Shav <shavenchew@yahoo.com>
2016-11-21 17:00:42 -0800

    'Hard Coding starter poll data in Mongo'

--- app.js
+++ app.js
@@ -5,9 +5,32 @@ var express = require('express'),
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static(__dirname + '/public'));
+
+
+//mongoose/mongo connection
+mongoose.connect('mongodb://localhost/polls');
+
+var pollSchema = new mongoose.Schema({
+  pollName : String
+});
+
+var Polls = mongoose.model('Poll', pollSchema);
+
+Polls.create({pollName : 'Favorite Super Villain'}, function(err, result){
+  if(err){
+    console.log(err);
+  } else {
+    console.log('Poll Successfully Added: ' + result);
+  }
+});
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('landing');
+});
+
+app.get('/polls', function(req, res){
+  res.render('polls');
 });
 
 app.listen('5000', function(){
