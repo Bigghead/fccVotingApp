476c4538d583d3678aed511ed948d572cea46546
Shav <shavenchew@yahoo.com>
2016-12-08 13:37:22 -0800

    Working Registration

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,7 +1,8 @@
 var express  = require('express'),
     mongoose = require('mongoose'),
     router   = express.Router(),
-    passport = require('passport');
+    passport = require('passport'),
+    User     = require('../models/userSchema.js');
 
 
 //========REGISTER ROUTES=======
@@ -9,9 +10,22 @@ router.get('/register', function(req, res){
   res.render('registerForm');
 });
 
-// router.post('/register', function(req, res){
-//   var
-// });
+router.post('/register', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+    User.register(new User({username: username}), password,
+    function(err, registeredUser){
+      if(err){
+        console.log(err);
+        res.render('resgisterForm');
+      } else {
+        passport.authenticate('local')(req, res, function(){
+          res.redirect('/polls');
+        });
+      }
+    });
+});
 
 
 //========LOGIN ROUTES=========
